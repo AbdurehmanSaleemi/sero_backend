46fe953d4ddd7a80548bb793a4fd12075e8fad50
Abdurehman Saleemi <76134547+AbdurehmanSaleemi@users.noreply.github.com>
2023-05-08 10:41:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
-import uploadImage from './image.js';
+//import uploadImage from './image.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
